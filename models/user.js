b809e86580bfb51d362966ab118971e6511efa8d
omerciftci <ciftcibasi23@gmail.com>
2022-09-19 10:36:50 +0300

    Db Relations Added

--- models/user.js
+++ models/user.js
@@ -11,6 +11,12 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
+      User.hasOne(models.RefreshToken, {
+        foreignKey: 'userId', targetKey: 'id'
+      });
+      User.hasMany(models.Todo, {
+        foreignKey: 'userId', targetKey: 'id'
+      });
     }
   }
   User.init({
